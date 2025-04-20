59a48315316526b16e4c18a0239eae7e6c4ed4fe
Weston Harper <13283469+wesharper@users.noreply.github.com>
2025-04-20 16:44:04 -0600

    Update root page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,34 @@
 <script lang="ts">
+  import WhLogo from "$lib/components/assets/wh-logo.svelte";
+  import { Header } from "$lib/components/header";
+  import { Sun, Moon } from "@lucide/svelte";
   import "../app.css";
-  import { ModeWatcher } from "mode-watcher";
+  import { ModeWatcher, toggleMode } from "mode-watcher";
 
   let { children } = $props();
 </script>
 
 <ModeWatcher />
-{@render children()}
+
+<div class="@container/layout size-full px-4 pt-20 pb-10">
+  <Header
+    class="bg-background/30 fixed top-4 right-4 left-4 mx-auto flex h-10 items-center justify-between rounded-lg px-3 py-2"
+  >
+    <a
+      class="hover:text-foreground/60 rounded-md p-1 transition-colors duration-100 ease-linear"
+      href="/"
+    >
+      <WhLogo />
+      <span class="sr-only">Go to home page</span>
+    </a>
+    <button
+      onclick={toggleMode}
+      class="hover:text-foreground/60 cursor-pointer rounded-md p-1 transition-colors duration-100 ease-linear"
+    >
+      <Sun class="hidden size-6 dark:block" />
+      <Moon class="size-6 dark:hidden" />
+      <span class="sr-only">Toggle theme</span>
+    </button>
+  </Header>
+  {@render children()}
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,10 @@
-<div class="relative mx-auto size-full p-28 pb-32 md:p-32 md:pb-36">
-  <div
-    class="absolute inset-20 bottom-24 flex animate-[pulse_5s_cubic-bezier(0.4,0,0.6,1)_infinite] bg-gradient-to-tr from-rose-400 via-amber-400 to-rose-400 md:inset-24 md:bottom-28 dark:from-fuchsia-600 dark:via-violet-600 dark:to-fuchsia-600"
-  ></div>
-  <main class="relative z-50 flex size-full flex-col justify-between">
+<div class="size-full pb-14 @md/layout:px-4 @xl/layout:px-10">
+  <main
+    class="relative flex size-full min-h-fit flex-col justify-between gap-4 p-4"
+  >
+    <div
+      class="absolute inset-0 -z-10 animate-[pulse_5s_cubic-bezier(0.4,0,0.6,1)_infinite] bg-gradient-to-tr from-rose-400 via-amber-400 to-rose-400 dark:from-fuchsia-600 dark:via-violet-600 dark:to-fuchsia-600"
+    ></div>
     <h1
       class="max-w-6xl text-2xl leading-relaxed font-extrabold sm:text-3xl md:text-4xl lg:text-5xl"
     >
