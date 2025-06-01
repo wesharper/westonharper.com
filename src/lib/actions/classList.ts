50414557f76869b1bca70cec4b46bd33ff168e63
Weston Harper <13283469+wesharper@users.noreply.github.com>
2025-06-01 17:45:14 -0600

    add classList action for adding classes to svelte:body

---
+++ src/lib/actions/classList.ts
@@ -0,0 +1,26 @@
+import clsx, { type ClassValue } from "clsx";
+import type { Action } from "svelte/action";
+
+function clsxList(input: ClassValue) {
+  return clsx(input).split(" ").filter(Boolean);
+}
+
+export const classList: Action<HTMLElement, ClassValue> = (
+  node: HTMLElement,
+  input: ClassValue,
+) => {
+  let classes = clsxList(input);
+
+  node.classList.add(...classes);
+
+  return {
+    update(input: ClassValue) {
+      node.classList.remove(...classes);
+      classes = clsxList(input);
+      node.classList.add(...classes);
+    },
+    destroy() {
+      node.classList.remove(...classes);
+    },
+  };
+};
