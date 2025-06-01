003b94a5706135f67693578babf52f9a4e5f19eb
Weston Harper <13283469+wesharper@users.noreply.github.com>
2025-06-01 17:07:48 -0600

    use newly styled button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,7 @@
+<script lang="ts">
+  import { Button } from "$ui/button";
+</script>
+
 <div class="size-full pb-14 @md/layout:px-4 @xl/layout:px-10">
   <main
     class="relative flex size-full min-h-fit flex-col justify-between gap-4 p-4"
@@ -8,13 +12,17 @@
     <h1
       class="max-w-6xl text-2xl leading-relaxed font-extrabold sm:text-3xl md:text-4xl lg:text-5xl"
     >
-      Hi, I'm Wes. I'm a software engineer, infrequent blogger, and serial
-      idea-haver.
+      Hi, I'm Wes. I'm a software engineer, <Button
+        variant="link"
+        size="link"
+        href="/blog">infrequent blogger</Button
+      >, and serial idea-haver.
     </h1>
-    <a
+    <Button
       href="mailto:hello@westonharper.com"
-      class="w-fit underline transition duration-300 ease-out hover:opacity-50 sm:text-lg md:text-xl dark:hover:opacity-70"
-      >Slide into my DMs</a
+      variant="link"
+      size="link"
+      class="sm:text-lg md:text-xl">Slide into my DMs</Button
     >
   </main>
 </div>
